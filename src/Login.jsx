739010e764158c7d89d94ffd4911aa2573329476
Shreyas <sonureigns007@gmail.com>
2022-12-01 10:06:10 +0530

    changes added

--- src/Login.jsx
+++ src/Login.jsx
@@ -14,23 +14,17 @@ const Login = () => {
                         <h1>Welcome Back</h1>
                         <div><TextField id="input-with-sx" label="Email" variant="standard" /></div>
                         <div><TextField id="input-with-sx" label="Password" variant="standard" /></div>
-                        <a href="" id="button"><Button disabled>Forgot Password</Button></a> 
+                        <a href="" id="button"><Button disabled>Forgot Password</Button></a>
                         <a  id="sign">Sign IN</a>
-
-
-
-
                     </aside>
                     <aside id='signup_part'>
                         <div id="signup_back">
-                           <h3>New here? </h3> 
-                           <h3>Sign Up and discover great</h3> 
+                           <h3>New here? </h3>
+                           <h3>Sign Up and discover great</h3>
                            <h3>amount of new oppurtunites</h3>
 
                            <a href="" ><Button variant="outlined">Sign Up</Button></a>
-                 
                         </div>
-                    
                     </aside>
 
 
