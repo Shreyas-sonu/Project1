0d3a0e53dd25ddb1f48b31e38532ba93c8b6bbc6
Shreyas <sonureigns007@gmail.com>
2022-12-02 10:27:58 +0530

    register added

--- src/auth/register/RegImage.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import Styles from "./register.module.css";
-
-const RegImage = () => {
-  return <aside className={Styles.imageBlock}></aside>;
-};
-
-export default RegImage;
